d651ed8e51fd2a6a692b4dc5ab71497026f3ae74
AmanKas <aman312000@gmail.com>
2023-01-15 13:16:18 +0530

    Left and Right Side Bar Updated

--- src/components/RightSidebar/Widget.jsx
+++ src/components/RightSidebar/Widget.jsx
@@ -1,24 +1,55 @@
 import React from 'react'
 import './RightSidebar.css'
-import comment from '../../assets/comment.svg'
+import comment from '../../assets/comment.svg' 
 import pen from '../../assets/pen.svg'
 import blackLogo from '../../assets/blackLogo.svg'
 
 const Widget = () => {
   return (
     <div className = 'widget'>
-        <h4>The Atom-D Overflow</h4>
+        <h4>The Atom-D Blog</h4>
         <div className='right-sidebar-div-1'>
             <div className='right-sidebar-div-2'>
                 <img src={pen} alt = "open" width = '18'/>
-                <p>Observability is key to the future of software (and your DevOps career)</p>
+                <p>Stack Gives Back 2022!</p>
             </div> 
             <div className='right-sidebar-div-2'>
             <img src={pen} alt = "open" width = '18'/>
-            <p>Podcast 374: How valuable is your screen name?</p>
-        </div>   
+            <p>Commit to something big: all about monorepos (Ep. 527)</p>
+            </div>   
+        </div>
+        <h4>Featured on Meta</h4>
+        <div className='right-sidebar-div-1'>
+            <div className='right-sidebar-div-2'>
+                <img src={comment} alt = "open" width = '18'/>
+                <p>2022 Community-a-thon Recap</p>
+            </div> 
+            <div className='right-sidebar-div-2'>
+            <img src={blackLogo} alt = "open" width = '18'/>
+            <p>Temporary policy: ChatGPT is banned</p>
+            </div>
+            <div className='right-sidebar-div-2'>
+            <img src={blackLogo} alt = "open" width = '18'/>
+            <p>The [shipping] tag is being burninated</p>
+            </div>
+        </div>
+        <h4>Hot Meta Posts</h4>
+        <div className='right-sidebar-div-1'>
+            <div className='right-sidebar-div-2'>
+                <p>34</p>
+                <p>Why was this spam flag declined, yet <br/> the question marked as spam? </p>
+            </div> 
+            <div className='right-sidebar-div-2'>
+                <p>45</p>
+                <p>What is the best course of action when <br/>a user has high enough rep to..</p>
+            </div>
+            <div className='right-sidebar-div-2'>
+                <p>69</p>
+                <p>Is a link to the "How to ask" help page a useful comment?</p>
+            </div>
         </div>
     </div>
+    
   )
 }
 
--- src/components/RightSidebar/WidgetTags.jsx
+++ src/components/RightSidebar/WidgetTags.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import './RightSidebar.css'
 
 const WidgetTags = () => {
+
+    const tags = ['c','cpp','css','express','firebase','html','java','javascript','mern','mysql','next.js','node.js','php' ,'python','reactjs']
+
   return (
-    <div>
-      
+    <div className='widget-tags'>
+      <h3>Watched tags</h3>
+      <div className='widget-tags-div'>
+      {
+        tags.map((tag) =>(
+            <p key = {tag}>{tag}</p>
+        ))
+      }
+      </div>
     </div>
   )
 }
