e23b19e079091b8e6f4f8e6cb98612d7df387f7f
AmanKas <aman312000@gmail.com>
2023-02-10 23:05:29 +0530

    logout and security

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,34 @@
 import React , {useEffect}from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { setCurrentUser } from '../../actions/currentUser'
+import { useSelector, useDispatch } from 'react-redux'
+import decode from 'jwt-decode'
+
 import logo from '../../assets/logo.png'
 import search from '../../assets/search.svg'
 import Avatar from '../../components/Avatar/Avatar'
 import './Navbar.css'
-import { setCurrentUser } from '../../actions/currentUser'
-import { useSelector, useDispatch } from 'react-redux'
 
 
 const Navbar = () => {
     const dispatch = useDispatch()
     var User = useSelector((state) => (state.currentUserReducer))
-    
+    const Navigate = useNavigate()
+
+    const handleLogout = () => {
+        dispatch({type: 'LOGOUT'});
+        Navigate('/')
+        dispatch(setCurrentUser(null))
+    }
+
     useEffect(()=>{
+        const token = User?.token
+        if (token){
+            const decodedToken = decode(token)
+            if(decodedToken.exp * 1000 < new Date().getTime()){
+                handleLogout()
+            }
+        }
         dispatch(setCurrentUser( JSON.parse(localStorage.getItem('Profile'))))
     },[dispatch])
 
@@ -35,7 +51,7 @@ const Navbar = () => {
                         <Link to ='/Auth' className = 'nav-item nav-links'>Log in</Link> :
                         <>
                            <Avatar backgroundColor= '#009dff' px = "10px" py = "7px" borderRadius= "50%" color='white' > <Link to = "/User" style = {{color:"white", textDecoration:'none'}}>{User.result.name.charAt(0).toUpperCase()}</Link></Avatar>
-                            <button className='nav-item nav-links'>Log out</button>
+                            <button className='nav-item nav-links' onClick={handleLogout}>Log out</button>
                         </> 
                     }
 
