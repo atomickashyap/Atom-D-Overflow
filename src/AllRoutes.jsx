325e08630b5f5823667bb667368b5e8633c3de03
AmanKas <aman312000@gmail.com>
2023-02-11 23:05:52 +0530

    Tags Pages Done

--- src/AllRoutes.jsx
+++ src/AllRoutes.jsx
@@ -5,16 +5,20 @@ import Auth from './pages/Auth/Auth';
 import Questions from './pages/Questions/Questions';
 import DisplayQuestion from './pages/Questions/DisplayQuestion';
 import AskQuestions from './pages/AskQuestions/AskQuestions';
+import Tags from './pages/Tags/Tags'
 
 
 const AllRoutes = () =>{
     return(
             <Routes>
+                <Route path='/' element = {<Home />} />
                 <Route exact path='/auth' element =  {<Auth />} />
                 <Route path = '/questions' element={<Questions />}/>
-                <Route path = '/questions/:id' element={<DisplayQuestion/>}/>
                 <Route path='/AskQuestions' element = {<AskQuestions />} />
-                <Route path='/' element = {<Home />} />
+                <Route path = '/questions/:id' element={<DisplayQuestion/>}/>
+                <Route path = '/Tags' element={<Tags/>}/>
+                
+                
                 
             </Routes>
     )
